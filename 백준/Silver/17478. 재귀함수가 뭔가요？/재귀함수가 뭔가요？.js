93b47050e53dabc8f62e5a8ce5bae218777567a4
Lee Soyeon <84645739+leeyirie@users.noreply.github.com>
2024-04-10 12:14:49 +0900

    [Silver V] Title: 재귀함수가 뭔가요?, Time: 144 ms, Memory: 9684 KB -BaekjoonHub

---
+++ 백준/Silver/17478. 재귀함수가 뭔가요？/재귀함수가 뭔가요？.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim();
+const N = parseInt(input, 10); // 입력 받은 재귀 횟수 N
+
+function chatbot(depth, N) {
+    const line = "____".repeat(depth);
+    console.log(`${line}"재귀함수가 뭔가요?"`);
+
+    if (depth === N) {
+        console.log(`${line}"재귀함수는 자기 자신을 호출하는 함수라네"`);
+    } else {
+        console.log(`${line}"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.`);
+        console.log(`${line}마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.`);
+        console.log(`${line}그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어."`);
+        chatbot(depth + 1, N); // 재귀 호출
+    }
+
+    console.log(`${line}라고 답변하였지.`);
+}
+
+console.log("어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다.");
+chatbot(0, N);
