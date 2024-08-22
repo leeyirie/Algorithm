154ca4c09634d59634eb91c50f582030a8b86bcc
Lee Soyeon <84645739+leeyirie@users.noreply.github.com>
2024-08-22 14:34:53 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 292 ms, Memory: 56808 KB -BaekjoonHub

---
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
@@ -0,0 +1,26 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+def solution(N,tree):
+	q = deque([1])
+	parent = [0] * (N+1)
+	while q:
+		now = q.popleft()
+		for i in tree[now]:
+			if parent[i] == 0 and i != 1:
+				parent[i] = now
+				q.append(i)
+	for i in range(2,N+1):
+		print(parent[i])
+
+if __name__ == "__main__":
+	N = int(input())
+	tree = dict()
+	for i in range(1,N+1):
+		tree[i] = []
+	for _ in range(N-1):
+		n1,n2 = map(int,input().split())
+		tree[n1].append(n2)
+		tree[n2].append(n1)
+	solution(N,tree)
